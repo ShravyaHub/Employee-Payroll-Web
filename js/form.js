071c4fec414fed7a63c9780bf3923a935947bc73
ShravyaHub <shravyak741@gmail.com>
2020-12-04 19:28:54 +0530

    ADDED: code to det event listener on salary range to display appropriate value and validation of name and date

--- js/form.js
+++ js/form.js
@@ -1,24 +1,25 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+    const name = document.querySelector('#name');
+    const textError = document.querySelector('.text-error');
+    name.addEventListener('input', function() {
+        if(name.value.length == 0) {
+            textError.textContent = "";
+            return;
+        }
+        try {
+            (new EmployeePayrollData()).name = name.value;;
+            textError.textContent = "";
+        } catch (e) {
+            textError.textContent = e;
+        }
+    });
+});
+
 const salary = document.querySelector('#salary');
-        const output = document.querySelector('.salary-output');
-        output.textContent = salary.value;
-        salary.addEventListener('input', function() {
-            output.textContent = salary.value;
-        });
-
-const text = document.querySelector('#name');
-const textError = document.querySelector('.text-error');
-text.addEventListener('input', function() {
-    let nameRegex = /^[A-Z][a-z]{2,}$/;
-    if(nameRegex.test(text.value)) {
-        textError.textContent = "";
-        document.getElementById("submitButton").disabled = false;
-
-    }
-    else {
-         textError.textContent = "Name is incorrect";
-         document.getElementById("submitButton").disabled = true;
-
-    }
+const output = document.querySelector('.salary-output');
+output.textContent = salary.value;
+salary.addEventListener('input', function() {
+    output.textContent = salary.value;
 });
 
 document.getElementById("submitButton").onclick = function() {
